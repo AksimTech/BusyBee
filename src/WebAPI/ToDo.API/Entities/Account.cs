7ef30a97b56ff9ff3e0a4b86837d0cb36e28d6be
aksimtech <aksimtech@gmail.com>
2020-09-29 16:36:06 +0300

    feat(todo.api): created Account, TodoItem

---
+++ src/WebAPI/ToDo.API/Entities/Account.cs
@@ -0,0 +1,8 @@
+namespace ToDo.API.Entities
+{
+    internal class Account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/WebAPI/ToDo.API/Entities/TodoItem.cs
@@ -0,0 +1,10 @@
+namespace ToDo.API.Entities
+{
+    internal class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+        public int AccountId { get; set; }
+    }
+}
