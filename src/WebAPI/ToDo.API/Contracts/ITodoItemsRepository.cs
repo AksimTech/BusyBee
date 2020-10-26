96c007855616016b79ade334e8dea2ed69977b54
aksimtech <aksimtech@gmail.com>
2020-10-26 14:52:15 +0300

    fixed(repo): rename ITodoItemRepository

--- src/WebAPI/ToDo.API/Contracts/ITodoItemsRepository.cs
+++ src/WebAPI/ToDo.API/Contracts/ITodoItemsRepository.cs
@@ -3,7 +3,7 @@ using ToDo.API.Entities;
 
 namespace ToDo.API.Contracts
 {
-    internal interface ITodoItemRepository {
+    internal interface ITodoItemsRepository {
         IEnumerable<TodoItem> FindAll();
         void Create(TodoItem todoItem);
         void Update(TodoItem todoItem);
