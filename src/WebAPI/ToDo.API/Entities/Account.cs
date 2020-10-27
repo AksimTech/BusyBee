605768fc0052f4a7cdc842aa0106be6c4fceb768
aksimtech <aksimtech@gmail.com>
2020-10-27 11:15:28 +0300

    feat(context): added attributes for entities

--- src/WebAPI/ToDo.API/Entities/Account.cs
+++ src/WebAPI/ToDo.API/Entities/Account.cs
@@ -1,8 +1,14 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace ToDo.API.Entities
 {
     internal class Account
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+
+        ICollection<TodoItem> TodoItems { get; set; }
     }
 }
--- src/WebAPI/ToDo.API/Entities/TodoItem.cs
+++ src/WebAPI/ToDo.API/Entities/TodoItem.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ToDo.API.Entities
 {
     internal class TodoItem
     {
+        [Key]
         public int Id { get; set; }
         public string Title { get; set; }
         public bool IsCompleted { get; set; }
+
+        [ForeignKey("AccountId")]
         public int AccountId { get; set; }
     }
 }
