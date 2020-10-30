ad48a6692f30146e7a1dc616bf721d40b3d6d793
aksimtech <aksimtech@gmail.com>
2020-10-30 15:03:40 +0300

    temp

---
+++ src/WebAPI/ToDo.API/GraphQL/GraphQLSchema/AppSchema.cs
@@ -0,0 +1,12 @@
+using GraphQL.Types;
+
+namespace ToDo.API.GraphQL.GraphQLSchema
+{
+    public class AppSchema : Schema {
+        public AppSchema()
+            :base()
+        {
+            
+        }
+    }
+}
